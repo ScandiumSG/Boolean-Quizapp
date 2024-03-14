dd75c176a5d1bccec5f961801b1a4bb694402370
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-14 11:47:03 +0100

    Content + Navigation - Added ContentView and NavigationBar components

---
+++ src/components/ContentView/ContentView.jsx
@@ -0,0 +1,13 @@
+import "./ContentView.css"
+
+const ContentView = () => {
+    return (
+        <div className="background spaced-background">
+            <div className="content-view-container">
+                hello
+            </div>
+        </div>
+    )
+}
+
+export default ContentView
---
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,13 @@
+import "./NavigationBar.css"
+
+const NavigationBar = () => {
+    return (
+        <div className="navigationbar-container">
+            <div>Hello</div>
+            <div>Hello</div>
+            <div>Hello</div>
+        </div>
+    )
+}
+
+export default NavigationBar
