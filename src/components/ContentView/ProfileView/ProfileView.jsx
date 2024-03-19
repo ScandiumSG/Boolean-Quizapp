13949e80e22df8be62de5d904ecf2279c6e03656
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-03-19 08:54:50 +0100

    Set Up Simple ProfileView Component

--- src/components/ContentView/ProfileView/ProfileView.jsx
+++ src/components/ContentView/ProfileView/ProfileView.jsx
@@ -1,17 +1,21 @@
-import "./ProfileView.css"
-import { useContext } from "react"
-import { userContext } from "@/contexts/contexts"
+import "./ProfileView.css";
+import { useContext } from "react";
+import { userContext } from "@/contexts/contexts";
 
 const ProfileView = () => {
-    const { user } = useContext(userContext)
+  const { user } = useContext(userContext);
 
-    return (
-        <div className="profile-view-container">
-            <p>
-                Hello, this is the profile page for {user.name}
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="profile-view-container content-frame">
+      <h2 className="page-header">Your Account</h2>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" value={user.username} disabled={true} />
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" value={user.name} disabled={true} />
+      <label htmlFor="email">Email</label>
+      <input id="name" type="email" value={user.email} disabled={true} />
+    </div>
+  );
+};
 
-export default ProfileView
+export default ProfileView;
