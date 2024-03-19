624c05dd13f0207bca53cff63467c65b186ab146
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-03-19 08:56:02 +0100

    Added More Mock Fields to UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import NavigationBar from './components/NavigationBar/NavigationBar'
-import ContentView from './components/ContentView/ContentView'
-import { userContext } from '@/contexts/contexts'
+import { useState } from "react";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import ContentView from "./components/ContentView/ContentView";
+import { userContext } from "@/contexts/contexts";
 
 const placeholderUser = {
   name: "Some Guy",
-
-}
+  email: "example@email.com",
+  username: "iamverysmart",
+};
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(placeholderUser)
+  const [currentUser, setCurrentUser] = useState(placeholderUser);
 
   return (
     <>
-    <userContext.Provider 
-      value={{user: currentUser, setUser: setCurrentUser}}
-    >
-      <NavigationBar/>
-      <ContentView />
-    </userContext.Provider>
+      <userContext.Provider
+        value={{ user: currentUser, setUser: setCurrentUser }}
+      >
+        <NavigationBar />
+        <ContentView />
+      </userContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
