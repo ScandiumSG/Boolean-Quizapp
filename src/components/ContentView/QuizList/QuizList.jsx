db8dda5b5f33ac122e1ac4384a7268c5ed42d57a
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-20 09:38:57 +0100

    QuizList - Retrieves data from backend API

--- src/components/ContentView/QuizList/QuizList.jsx
+++ src/components/ContentView/QuizList/QuizList.jsx
@@ -1,43 +1,47 @@
 import "./QuizList.css";
 import QuizListItem from "./QuizListItem";
+import { useEffect, useState } from "react"
 
 function QuizList() {
+  const [quizList, setQuizList] = useState([])
+
+  const fetchQuizList = async () => {
+    const request = {
+      method: "GET",
+      headers: {
+        "content-type": "application/json"
+      }
+    }
+    await fetch("https://bobbb.azurewebsites.net/play/quiz/", request)
+      .then((res) => res.json())
+      .then((res) => res.data)
+      .then((res) => console.log(res))
+      .then((res) => setQuizList([...res]))
+  }
+
+  useEffect(() => {
+    fetchQuizList()
+  }, [])
+
+  if (quizList.length === 0) {
+    return (<div style={{display: "flex", marginTop: "50px"}}>Loading...</div>)
+  }
+
   return (
     <>
-      <h1 className="page-header">The Web's #1 Quiz App</h1>
+      <h1 className="page-header">The Web&apos;s #1 Quiz App</h1>
       <h2 className="menu-header">Latest Quizzes</h2>
       <div className="menu-panel">
         <div className="quiz-list">
-          <QuizListItem
-            title="Rare insect Species of the Amazon Rainforest"
-            description="Lorem ipsum dolor sit amet."
-            numQuestions="10"
-            authorName="Edward James"
-          />
-          <QuizListItem
-            title="Harry Potter Lore"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            numQuestions="55"
-            authorName="Lilly Smith"
-          />
-          <QuizListItem
-            title="Pacific Island Nations"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            numQuestions="22"
-            authorName="Zack"
-          />
-          <QuizListItem
-            title="Wine Districts in Northern Spain"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            numQuestions="5"
-            authorName="Jimmy Nelson"
-          />
-          <QuizListItem
-            title="C# Fundamentals"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-            numQuestions="41"
-            authorName="Morgan Brown"
-          />
+          {quizList.map((quizItem, index) => 
+            <QuizListItem
+              key={index}
+              title={quizItem.title}
+              description={quizItem.description}
+              numQuestions={quizItem.length}
+              authorName={quizItem.userId}
+            />
+          )}
         </div>
       </div>
     </>
