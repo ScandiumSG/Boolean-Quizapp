f60ba860b842634571e6e50412e24e1d3b2d316c
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-20 13:50:05 +0100

    AccountIcon - Now handle non-logged in users

--- src/components/Common/AccountIcon/AccountIcon.jsx
+++ src/components/Common/AccountIcon/AccountIcon.jsx
@@ -1,17 +1,30 @@
+import { loginContext } from "@/contexts/contexts"
 import "./AccountIcon.css"
 import PropTypes from 'prop-types'
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
 
 const AccountIcon = ({ user }) => {
     const navigate = useNavigate()
+    const { setShowLogin } = useContext(loginContext)
 
     const handleClick = () => {
         navigate("/profile")
     }
 
+    if (!user) {
+        return (
+            <div className="account-login-container">
+                <button onClick={() => setShowLogin(true)}>
+                    <span>Login</span>
+                </button>
+            </div>
+        )
+    }
+
     return (
         <div className="account-icon-container" onClick={() => handleClick()}>
-            {user.name.substring(0,1) + " " + user.name.split(" ")[1].substring(0,1)}
+            {user.username.substring(0,1).toUpperCase()}
         </div>
     )
 }
