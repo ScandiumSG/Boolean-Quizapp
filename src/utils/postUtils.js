d6823e2fb7152ce888bb1adcd6bed25e40ef23cd
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-21 15:22:04 +0100

    CreateQuiz - Added better error handling

--- src/utils/postUtils.js
+++ src/utils/postUtils.js
@@ -1,15 +1,20 @@
 export const validateData = (data) => {
+    const errorData = []
     if (data.title === "" || data.description === "") {
-        return ("Both title and description must be filled inn.")
-    } else if (data.questions.some((question) => question.text === "")) {
-        return ("Question text can't be empty.")
-    } else {
-        if (data.questions
-            .every((question) => !(question.answerOptions.some(answer => answer.isCorrect)))) 
-        { return ("Each question must have atleast one (1) correct answer.")}
-        if (data.questions
-            .every((question) => (question.answerOptions.some(answer => answer.text === "")))) 
-        { return ("Each answer alternative must contain an answer.")}
+        errorData.push("Both title and description must be filled inn.")
+    } 
+    if (data.questions.some((question) => question.text === "")) {
+        errorData.push("Question text can't be empty.")
     }
-    return "";
+
+    if (data.questions.every((question) => 
+        !(question.answerOptions.some(answer => answer.isCorrect)))) { 
+            errorData.push("Each question must have atleast one (1) correct answer.")
+    }
+    if (data.questions.every((question) => 
+        (question.answerOptions.some(answer => answer.text === "")))) { 
+        errorData.push("Each answer alternative must contain an answer.")
+    }
+
+    return errorData;
 }
