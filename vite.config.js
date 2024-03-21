b67249f9f65cb49e58c96b11fc90c8c660538297
Stian Gaustad <stian.gaustad@no.experis.com>
2024-03-21 10:39:56 +0100

    Config - Changed import of pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,47 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
-import { pwaManifest } from 'pwa.config'
+
+const pwaManifest = {
+  registerType:'prompt',
+  includeAssests:['bobs-brain-box.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Bobs Brain Box",
+    short_name:"BBB",
+    description:"Bobs Brainbox of quizzes, expand your mind!",
+    icons:[{
+      src: '/bobs-brain-box.ico',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/bobs-brain-box.ico',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/bobs-brain-box.ico',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/bobs-brain-box.ico',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/quiz",
+  orientation:'portrait'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
