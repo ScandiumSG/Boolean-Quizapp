91d4add25fd15866b3dcc477401b1fb11a090aaa
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-03-21 09:40:30 +0100

    Style - Fixed Style Issues Accross the Page

--- src/components/ContentView/QuizList/QuizList.jsx
+++ src/components/ContentView/QuizList/QuizList.jsx
@@ -1,47 +1,43 @@
 import { baseQuizUrl } from "@/utils/apiUtil";
 import "./QuizList.css";
 import QuizListItem from "./QuizListItem";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function QuizList() {
-  const [quizList, setQuizList] = useState([])
+  const [quizList, setQuizList] = useState([]);
 
   const fetchQuizList = async () => {
     const request = {
       method: "GET",
       headers: {
-        "content-type": "application/json"
-      }
-    }
+        "content-type": "application/json",
+      },
+    };
     await fetch(baseQuizUrl, request)
       .then((res) => res.json())
       .then((res) => res.data)
-      .then((res) => setQuizList([...res]))
-  }
+      .then((res) => setQuizList([...res]));
+  };
 
   useEffect(() => {
-    fetchQuizList()
-  }, [])
+    fetchQuizList();
+  }, []);
 
   if (quizList.length === 0) {
-    return (<div style={{display: "flex", marginTop: "50px"}}>Loading...</div>)
+    return <div style={{ display: "flex", marginTop: "50px" }}>Loading...</div>;
   }
 
   return (
     <>
-      <h1 className="page-header">The Web&apos;s #1 Quiz App</h1>
+      <h1 id="quiz-list-page-header" className="page-header">
+        Try Other Users&apos; Quiz!
+      </h1>
       <h2 className="menu-header">Latest Quizzes</h2>
       <div className="menu-panel">
         <div className="quiz-list">
-          {quizList.map((quizItem, index) => 
-            <QuizListItem
-              key={index}
-              title={quizItem.title}
-              description={quizItem.description}
-              numQuestions={quizItem.length}
-              authorName={quizItem.userName}
-            />
-          )}
+          {quizList.map((quizItem, index) => (
+            <QuizListItem key={index} title={quizItem.title} description={quizItem.description} numQuestions={quizItem.length} authorName={quizItem.userName} />
+          ))}
         </div>
       </div>
     </>
--- src/components/ContentView/RegisterUser/RegisterUser.jsx
+++ src/components/ContentView/RegisterUser/RegisterUser.jsx
@@ -1,104 +1,86 @@
-import { userRegisterUrl } from "@/utils/apiUtil"
-import "./RegisterUser.css"
-import { useContext, useState } from 'react'
-import { useNavigate } from "react-router-dom"
-import { loginContext } from "@/contexts/contexts"
+import { userRegisterUrl } from "@/utils/apiUtil";
+import "./RegisterUser.css";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginContext } from "@/contexts/contexts";
 
 const RegisterUser = () => {
-    const [profileData, setProfileData] = useState({
-        email: "",
-        username: "",
-        password: "",
-        role: 0,
-    })
-    const [errorMessage, setErrorMessage] = useState("")
-    const navigate = useNavigate()
-    const { setShowLogin } = useContext(loginContext)
+  const [profileData, setProfileData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    role: 0,
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const { setShowLogin } = useContext(loginContext);
 
-    const handleChange = (e) => {
-        setProfileData({...profileData, [e.target.id]: e.target.value})
+  const handleChange = (e) => {
+    setProfileData({ ...profileData, [e.target.id]: e.target.value });
+  };
+
+  const handleRegister = async () => {
+    let error = "";
+    let errorTriggered = false;
+    if (profileData.email === "") {
+      error = "Email address is required.\n";
+      errorTriggered = true;
+    }
+    if (profileData.username === "") {
+      error += "Username is required.\n";
+      errorTriggered = true;
+    }
+    if (profileData.password.length < 6) {
+      error += "Password must be atleast 6 characters.\n";
+      errorTriggered = true;
+    }
+    if (errorTriggered) {
+      setErrorMessage(error);
+      return;
     }
 
-    const handleRegister = async () => {
-        let error = ""
-        let errorTriggered = false
-        if (profileData.email === "") {
-            error = "Email address is required.\n"
-            errorTriggered = true
-        }
-        if (profileData.username === "") {
-            error += "Username is required.\n"
-            errorTriggered = true
-        }
-        if (profileData.password.length < 6) {
-            error += "Password must be atleast 6 characters.\n"
-            errorTriggered = true
-        }
-        if (errorTriggered) {
-            setErrorMessage(error)
-            return;
-        }
+    const request = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(profileData),
+    };
 
-        const request = {
-            method: "POST", 
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(profileData)
+    await fetch(userRegisterUrl, request)
+      .then((res) => {
+        if (res.ok) {
+          navigate("/quiz");
+          setShowLogin(true);
+          return res.json();
+        } else {
+          console.log(res.json());
+          throw new Error("Could not register user", res);
         }
+      })
+      .catch((error) => console.log(error));
 
-        await fetch(userRegisterUrl, request)
-            .then((res) => {
-                if (res.ok) {
-                    navigate("/quiz")
-                    setShowLogin(true)
-                    return res.json()
-                } else {
-                    console.log(res.json())
-                    throw new Error("Could not register user", res)
-                }
-            })
-            .catch((error) => console.log(error))
+    // TODO: Register user,
+    // Must wait until backend supports users
+  };
 
-        // TODO: Register user,
-        // Must wait until backend supports users
-    }
+  return (
+    <div className="menu-panel standalone register-user-container">
+      <h2>Register new user account</h2>
+      <div className="register-user-fields">
+        <label>Email address*:</label>
+        <input name="user_email" id="email" type="email" value={profileData.email} onChange={(e) => handleChange(e)} />
+        <label>Username*:</label>
+        <input name="user_username" id="username" type="text" value={profileData.username} onChange={(e) => handleChange(e)} />
+        <label>Your password:</label>
+        <input name="user_password" id="password" type="password" value={profileData.password} onChange={(e) => handleChange(e)} />
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        <button onClick={() => handleRegister()}>
+          <span>Register</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className="register-user-container">
-            <h2>Register new user account</h2>
-            <div className="register-user-fields">
-                <label>Email address*:</label>
-                <input 
-                    name="user_email"
-                    id="email"
-                    type="email"
-                    value={profileData.email}
-                    onChange={(e) => handleChange(e)}
-                />
-                <label>Username*:</label>
-                <input 
-                    name="user_username"
-                    id="username"
-                    type="text"
-                    value={profileData.username}
-                    onChange={(e) => handleChange(e)}
-                    />
-                <label>Your password:</label>
-                <input 
-                    name="user_password"
-                    id="password"
-                    type="password"
-                    value={profileData.password}
-                    onChange={(e) => handleChange(e)}
-                />
-                {errorMessage && <p style={{color: "red"}}>{errorMessage}</p>}
-                <button onClick={() => handleRegister()}>
-                    <span>Register</span>
-                </button>
-            </div>
-        </div>
-    )
-}
-
-export default RegisterUser
+export default RegisterUser;
